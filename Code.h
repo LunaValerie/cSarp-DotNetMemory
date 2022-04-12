4f22e805460d8ecccbc1b11c4f52705824f7ed36
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-04-12 19:31:40 +0300

    Minecraft

---
+++ Code.h
@@ -0,0 +1,11 @@
+DotNetScanMemory_SmoLL Scan = new DotNetScanMemory_SmoLL();
+
+IntPtr[] Bulunan;
+
+MessageBox.Show(""); // Minecraft Titanyum Crack !
+
+this.Founds = this.Scan.ScanArray(this.Scan.GetPID("javaw"), "C3 F5 68 3F");
+
+this.Scan.WriteArray(this.Founds[0], "00 00 01 3F");
+
+MessageBox.Show(this.Founds[0].ToString("X"));
